018a187600128e513c73fba1efe767913cee4e72
Duyen Nguyen <113315119+dnguyennk@users.noreply.github.com>
2024-06-24 19:31:23 -0400

    Add files via upload

---
+++ C/memoryRepresentation.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    char myChar1 = 'a';
+    
+    printf("Character: %c \n", myChar1);                //Output: a
+    printf("Decimal ASCII Code: %d \n", myChar1);       //Output: 97
+    printf("Hexadecimal ASCII Code: %X \n", myChar1);   //Output: 61
+    
+    return 0;
+}
