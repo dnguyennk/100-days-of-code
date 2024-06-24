20cccf7b559f93ec0d5fd54a0307d66a73e6ab83
Duyen Nguyen <113315119+dnguyennk@users.noreply.github.com>
2024-06-24 19:33:48 -0400

    Update memoryRepresentation.c

--- C/memoryRepresentation.c
+++ C/memoryRepresentation.c
@@ -6,6 +6,12 @@ int main(){
     printf("Character: %c \n", myChar1);                //Output: a
     printf("Decimal ASCII Code: %d \n", myChar1);       //Output: 97
     printf("Hexadecimal ASCII Code: %X \n", myChar1);   //Output: 61
-    
+
+    char myChar2 = 'O';
+    printf("Character: %c \n", myChar1);                //Output: O
+    printf("Decimal ASCII Code: %d \n", myChar1);       //Output: 79
+    printf("Hexadecimal ASCII Code: %X \n", myChar1);   //Output: 4F
+
+    print
     return 0;
 }
